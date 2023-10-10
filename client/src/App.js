3f0d21af931e12b9c07d1d9ab95c34dfa5a99d26
JUSTFIDEL <fidenki1429@gmail.com>
2023-10-10 14:11:07 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+	return (
+		<div className='App'>
+			<header>
+				<div className='logo'>
+					<img src='../favicon.png'></img>
+					<a href='/'>JustFidel</a>
+				</div>
+			</header>
+			<main>
+				<h1>Products</h1>
+			</main>
+		</div>
+	)
+}
+
+export default App
